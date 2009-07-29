10b43de8311f96c99de08131df5b383dceba2913
openwave <shankar@openwavecomp.com>
2009-07-29 16:14:48 +0530

    search layout

---
+++ public/javascripts/homepage_photos.js
@@ -0,0 +1,42 @@
+   function showphoto1(w1URL) {
+        
+        document.getElementById('w1').innerHTML="<img id='w1Img' width='250' src='"+w1URL+"' alt='' />";
+        document.getElementById('w1Img').style.display='inline';
+      }
+	
+	   function showphoto2(w2URL) {
+        //alert(imgURL);
+        document.getElementById('w2').innerHTML="<img id='w2Img' width='260' src='"+w2URL+"' alt='' />";
+        document.getElementById('w2Img').style.display='inline';
+      }
+	     function showphoto3(w3URL) {
+        //alert(imgURL);
+        document.getElementById('w3').innerHTML="<img id='w3Img' width='250'  src='"+w3URL+"' alt='' />";
+        document.getElementById('w3Img').style.display='inline';
+      }
+	     function showphoto4(w4URL) {
+        //alert(imgURL);
+        document.getElementById('w4').innerHTML="<img id='w4Img' width='418'  src='"+w4URL+"' alt='' style= 'margin-bottom: 5px;' />";
+        document.getElementById('w4Img').style.display='inline';
+      }
+	     function showphoto5(w5URL) {
+        //alert(imgURL);
+        document.getElementById('w5').innerHTML="<img id='w5Img' width='418'  src='"+w5URL+"' alt='' />";
+        document.getElementById('w5Img').style.display='inline';
+      }
+	     function showphoto6(w6URL) {
+        //alert(imgURL);
+        document.getElementById('w6').innerHTML="<img id='w6Img' width='617'  src='"+w6URL+"' alt='' style='margin-top:-4px;' />";
+        document.getElementById('w6Img').style.display='inline';
+      }
+	     function showphoto7(w7URL) {
+        //alert(imgURL);
+        document.getElementById('w7').innerHTML="<img id='w7Img' width='195'  src='"+w7URL+"' alt='' />";
+        document.getElementById('w7Img').style.display='inline';
+      }  
+	     function showphoto8(w8URL) {
+        //alert(imgURL);
+        document.getElementById('w8').innerHTML="<img id='w8Img' width='184' src='"+w8URL+"' alt='' style='margin-right: 8px;' />";
+        document.getElementById('w8Img').style.display='inline';
+      }
+	  
