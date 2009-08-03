c4cf096da7976e6f6b2474f63be77b79d7c77522
openwave <shankar@openwavecomp.com>
2009-08-03 19:00:26 +0530

    S3 storage

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :org
   acts_as_geocodable
 
-  has_attached_file :image
   
+ has_attached_file :image,:styles => {:thumb   => "107x60#",:full    => "545x307>" },:storage => :s3,:s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "events-images/:attachment/:id/:style.:extension",  :bucket => 'hq_project_development_bucket' 
   AGE_TYPES = [[ "18+", "18+" ],
     [ "21+", "21+" ],
     [ "None", "None" ]]
