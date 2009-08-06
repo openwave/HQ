e6fedeb7aa2482f88d609bdc8b105003ca827969
openwave <shankar@openwavecomp.com>
2009-08-06 15:50:23 +0530

    tooltip js

---
+++ public/javascripts/vtip.js
@@ -0,0 +1,37 @@
+/**
+Vertigo Tip by www.vertigo-project.com
+Requires jQuery
+*/
+
+this.vtip = function() {    
+    this.xOffset = -10; // x distance from mouse
+    this.yOffset = 10; // y distance from mouse       
+    
+    jQuery(".vtip").unbind().hover(    
+        function(e) {
+            this.t = this.title;
+            this.title = ''; 
+            this.top = (e.pageY + yOffset); this.left = (e.pageX + xOffset);
+            
+            jQuery('body').append( '<p id="vtip"><img id="vtipArrow" />' + this.t + '</p>' );
+                        
+            jQuery('p#vtip #vtipArrow').attr("src", 'images/vtip_arrow.png');
+            jQuery('p#vtip').css("top", this.top+"px").css("left", this.left+"px").fadeIn("slow");
+            
+        },
+        function() {
+            this.title = this.t;
+            jQuery("p#vtip").fadeOut("slow").remove();
+        }
+    ).mousemove(
+        function(e) {
+            this.top = (e.pageY + yOffset);
+            this.left = (e.pageX + xOffset);
+                         
+            jQuery("p#vtip").css("top", this.top+"px").css("left", this.left+"px");
+        }
+    );            
+    
+};
+
+jQuery(document).ready(function($){vtip();}) 
