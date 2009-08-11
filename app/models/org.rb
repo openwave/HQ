96de0b70c40e60e053d083555f772c5792c6038c
openwave <shankar@openwavecomp.com>
2009-08-11 18:06:43 +0530

    org

--- app/models/org.rb
+++ app/models/org.rb
@@ -26,13 +26,15 @@ class Org < ActiveRecord::Base
 has_many :price_tabs
   
   # Paperclip
-  has_attached_file :image,:styles => {:thumb => "107x60",:full => "545x307"}
-  has_attached_file :image,:styles => {:thumb   => "107x60#",:full    => "545x307>" },:storage => :s3,:s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "orgs/:attachment/:id/:style.:extension",  :bucket => 'hq_project_development_bucket'
+ 
+  has_attached_file :image,:styles => {:original  => "280x200>" },:default_style => :original,:storage => :s3,:s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "orgs/:attachment/:id/:style.:extension",  :bucket => 'hq_project_development_bucket'
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/gif', 'image/png', 'image/pjpeg', 'image/x-png']
-#   validates_attachment_size :image, :greater_than => 4.kilobytes, :message => "minimum size is 4kB"
+  validates_attachment_presence :image,:if => :image_url_provided?, :message => 'is invalid or inaccessible'
+ #validates_attachment_size :image, :greater_than => 20.kilobytes, :message => "minimum size is 4kB"
+
   # using typical SLR camera aspect ratio of 2:3 (I.e. 4" x 6")
   #:styles => { :original => "700x466", :cover => "150x100!", :thumb => "68x50!" },
-  validates_uniqueness_of :title,:message=>'Already exists'
+  
   
   STATES = [[ "New Jersey", "NJ" ]]
 
@@ -56,31 +58,28 @@ has_many :price_tabs
     
   end
 
- def update_attributes(att)
+   
+def update_attributes(att)
 
   # Should we crop?
+ if !att.nil?
   if !att[:x1].nil?
   scaled_img = Magick::ImageList.new(self.image.url)
- # scaled_img = Magick::ImageList.new(self.image.path)
   orig_img = Magick::ImageList.new(self.image.url(:original))
-
-  #orig_img = Magick::ImageList.new(self.image.path(:original))
   scale = orig_img.columns.to_f / scaled_img.columns
-
-  args = [ att[:x1], att[:y1], att[:width], att[:height] ]
+  args = [ att[:x1].to_i, att[:y1].to_i, att[:width].to_i, att[:height].to_i ]
  args = args.collect { |a| a.to_i * scale }
-
  orig_img.crop!(*args)
-  puts "********************************************* PATH -URL********************************** #{self.image.url}"
-  puts "********************************************* PATH -PATH ********************************** #{self.image.path}"
-   puts "********************************************* PATH -PATH ********************************** #{orig_img}"
-  orig_img.write("./public/original.jpg")
-  #orig_img.write(self.image.path)
- # self.image.reprocess!
-  self.save
+ orig_img.write("./public/original.jpg")
+ puts "********************************************* PATH -URL********************************** #{self.image.url}"
+  puts "********************************************* PATH -PATH ********************************** #{att[:x1].to_i}, #{att[:y1].to_i}, #{att[:width].to_i},#{att[:height].to_i}"
+   self.save
   end
+ end
   super(att)
 end
+
+ 
 
 def upload_to_s3
    #has_attached_file :image,:styles => {:thumb   => "107x60#",:full    => "545x307>" },:storage => :s3,:s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "orgs/:attachment/:id/:style.:extension",  :bucket => 'hq_project_development_bucket'
@@ -90,7 +89,12 @@ def upload_to_s3
   AWS::S3::S3Object.store('original.jpg',open('./public/original.jpg'),'hq_project_development_bucket/orgs/images/' + self.id.to_s + '/', :access => :public_read)
     puts "************************************************* AFTER S3::store called *******************************"
 end
+private
 
+  def image_url_provided?
+     puts " ************************************ IMAGE URL VALIDATION CALLED ********************************"
+    !self.image.blank?
+  end
 
   
 end
